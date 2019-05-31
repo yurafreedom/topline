a9730a4457f407a02b9d31cc095e21a4dec02993
Yura Sachenko <yurasachenko@MacBook-Pro-Yura.local>
2019-05-31 17:07:35 +0300

    adaptive progress

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,84 @@
+var bodyScrollOptions = {
+    reserveScrollBarGap: true
+};
+
+function openModal(hrefModal) {
+    
+    if ($(hrefModal).length > 0){
+        $(hrefModal).fadeIn(300);
+    
+        bodyScrollLock.clearAllBodyScrollLocks();
+        bodyScrollLock.disableBodyScroll(hrefModal, bodyScrollOptions);
+    }
+    
+    $(hrefModal).find('.slick-slider').slick('setPosition');
+  
+}
+
+function closeModals() {
+  if (scrollbarWidth > 0) {
+    $('.popup-block:not(:hidden)').fadeOut(200, function() {
+            bodyScrollLock.clearAllBodyScrollLocks();
+        });
+  } else {
+    $('.popup-block:not(:hidden)').fadeOut(200);
+    
+    bodyScrollLock.clearAllBodyScrollLocks();
+  }
+}
+
+
+$(document.body).on('click','[data-toggle="modal"]',function(e) {
+  e.preventDefault();
+  
+  var hrefModal = $(this).attr('data-target');
+  
+  openModal(hrefModal);
+});
+
+$(document.body).on('click','.popup-block__overlay',function(e) {
+  var closeButton = $(this).children('[data-toggle="dismiss"]');
+  
+  if (e.target != this) {
+//    return false;
+  } else {
+    closeModals();
+  }
+});
+
+
+$(document.body).on('click','[data-toggle="dismiss"]',function(e) {
+  e.preventDefault();
+  
+  closeModals();
+});
+
+var block = $("<div>").css({
+        height: "50px",
+        width: "50px"
+    }),
+    indicator = $("<div>").css({
+        height: "200px"
+    }),
+    scrollbarWidth = 0;
+$("body").append(block.append(indicator));
+var w1 = $("div", block).innerWidth();
+block.css("overflow-y", "scroll");
+var w2 = $("div", block).innerWidth();
+$(block).remove();
+scrollbarWidth = (w1 - w2);
+
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $(this).toggleClass('active');
+  $("#header-menu").toggleClass("active");
+});
+
+$("#menu-close").click(function(e) {
+  e.preventDefault();
+  $("#header-menu").removeClass("active");
+});
+
 $('.directions-block__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -6,6 +87,12 @@ $('.directions-block__slider').slick({
     arrows: true,
     prevArrow: '<button class="prev directions-block__arrow"></button>',
     nextArrow: '<button class="next directions-block__arrow"></button>',
+    responsive: [{
+        breakpoint: 767,
+        settings: {
+            arrows: !false
+        }
+    }]    
 });
 
 $('.stories-block__slider').slick({
